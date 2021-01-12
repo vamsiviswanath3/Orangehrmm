80a3fdc9e81d33b3ba7634e154261e021eba5ec2
ssmc <ssmc@DESKTOP-EVD93CH>
2021-01-12 12:20:08 +0530

    Github

---
+++ src/main/java/Orange5/Orange5/App.java
@@ -0,0 +1,13 @@
+package Orange5.Orange5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
